927a6f1346765bfc2f162991e505660cce81a68f
Jose Murga <murga.jose16@gmail.com>
2024-09-20 17:08:37 -0500

    Bot with Auth estable

--- bots/dialogBot.js
+++ bots/dialogBot.js
@@ -21,6 +21,7 @@ class DialogBot extends ActivityHandler {
         this.dialog = dialog;
         this.dialogState = this.conversationState.createProperty('DialogState');
 
+
         this.onMessage(async (context, next) => {
             console.log('Running dialog with Message Activity.');
 
@@ -29,6 +30,89 @@ class DialogBot extends ActivityHandler {
 
             await next();
         });
+
+
+        /* this.onMessage((context, next) => __awaiter(this, void 0, void 0, function* () {
+
+            console.log('Running dialog with Message Activity.');
+
+            var _a;
+            var _b;
+            // check if user input is "/reset"
+            if (context.activity.text == "/reset") {
+                // reset conversation history
+                conversation_history_dict[context.activity.conversation.id] = [];
+                // send response to user
+                yield context.sendActivity(botbuilder_1.MessageFactory.text("Clearing session. Starting with new context - just ask your question."));
+                // By calling next() you ensure that the next BotHandler is run.
+                yield next();
+            }
+            else {
+                //construct conversation history from conversation_history_array
+                let tmp_conversation_history = "";
+                (_a = conversation_history_dict[_b = context.activity.conversation.id]) !== null && _a !== void 0 ? _a : (conversation_history_dict[_b] = [messages_init]);
+                let conversation_history_array = conversation_history_dict[context.activity.conversation.id];
+
+                // check if conversation history is not larger than history_length, if so remove from begining
+                if (count_user_messages(conversation_history_array) > history_length) {
+                    console.log("History too long - removing first element");
+                    let N = 2; // removing two elements (question and answer)
+                    for (let i = 0; i < N; i++) {
+                        conversation_history_array.shift(); // remove the first element from the array
+                    }
+                    // make sure that the first element is always the initial message (system message)
+                    conversation_history_array[0] = messages_init;
+                }
+
+                // Add the user input to the conversation history
+                conversation_history_array.push({ "role": "user", "content": context.activity.text });
+
+                let reqBody = JSON.stringify({
+                    "data_sources": [
+                        {
+                            "type": "AzureCognitiveSearch",
+                            "parameters": {
+                                "endpoint": process.env.SEARCH_ENDPOINT,
+                                "key": process.env.SEARCH_KEY,
+                                "index_name": process.env.SEARCH_INDEX_NAME
+                            }
+                        }
+                    ],
+                    "messages": conversation_history_array,
+                    "temperature": 0.7,
+                    "top_p": 0.95,
+                    "frequency_penalty": 0,
+                    "presence_penalty": 0,
+                    "max_tokens": 800,
+                    "stop": null
+                });
+
+                try {
+                    // Send request to openai
+                    const data = yield postDataToEndpoint(url, reqBody, headers);
+                    // Add the chatbot response to the conversation history
+                    conversation_history_array.push({ "role": data.choices[0].message.role, "content": data.choices[0].message.content });
+                    // Update conversation history
+                    conversation_history_dict[context.activity.conversation.id] = conversation_history_array;
+                    // Send response to user
+                    const replyText = `${data.choices[0].message.content} \n[~  ${data.usage.total_tokens} tokens in ${conversation_history_array.length} turns]`;
+
+                    // const replyText = `Echox: ${ context.activity.text } value: ${ context.activity.value }`;
+                    yield context.sendActivity(botbuilder_1.MessageFactory.text(replyText));
+                    //yield this.dialog.run(context, this.dialogState);
+
+                    // By calling next() you ensure that the next BotHandler is run.
+                    yield next();
+                }
+                catch (error) {
+                    console.log(error);
+                    yield context.sendActivity(botbuilder_1.MessageFactory.text(`${error} - try again later!`));
+                    yield next();
+                }
+            }
+        })); 
+        */
+
     }
 
     /**
--- index.js
+++ index.js
@@ -15,11 +15,11 @@ const restify = require('restify');
 // Import required bot services.
 // See https://aka.ms/bot-services to learn more about the different parts of a bot.
 const {
-    CloudAdapter,
-    ConversationState,
-    MemoryStorage,
-    UserState,
-    ConfigurationBotFrameworkAuthentication
+  CloudAdapter,
+  ConversationState,
+  MemoryStorage,
+  UserState,
+  ConfigurationBotFrameworkAuthentication
 } = require('botbuilder');
 
 const { AuthBot } = require('./bots/authBot');
@@ -31,25 +31,17 @@ const botFrameworkAuthentication = new ConfigurationBotFrameworkAuthentication(p
 const adapter = new CloudAdapter(botFrameworkAuthentication);
 
 adapter.onTurnError = async (context, error) => {
-    // This check writes out errors to console log .vs. app insights.
-    // NOTE: In production environment, you should consider logging this to Azure
-    //       application insights. See https://aka.ms/bottelemetry for telemetry
-    //       configuration instructions.
-    console.error(`\n [onTurnError] unhandled error: ${ error }`);
 
-    // Send a trace activity, which will be displayed in Bot Framework Emulator
-    await context.sendTraceActivity(
-        'OnTurnError Trace',
-        `${ error }`,
-        'https://www.botframework.com/schemas/error',
-        'TurnError'
-    );
+  console.error(`\n [onTurnError] unhandled error: ${error}`);
 
-    // Send a message to the user
-    await context.sendActivity('The bot encountered an error or bug.');
-    await context.sendActivity('To continue to run this bot, please fix the bot source code.');
-    // Clear out state
-    await conversationState.delete(context);
+  // Send a trace activity, which will be displayed in Bot Framework Emulator
+  await context.sendTraceActivity('OnTurnError Trace', `${error}`, 'https://www.botframework.com/schemas/error', 'TurnError');
+
+  // Send a message to the user
+  await context.sendActivity('The bot encountered an error or bug.');
+  await context.sendActivity('To continue to run this bot, please fix the bot source code.');
+  // Clear out state
+  await conversationState.delete(context);
 };
 
 // Define the state store for your bot.
@@ -63,6 +55,7 @@ const userState = new UserState(memoryStorage);
 
 // Create the main dialog.
 const dialog = new MainDialog();
+
 // Create the bot that will handle incoming messages.
 const bot = new AuthBot(conversationState, userState, dialog);
 
@@ -70,20 +63,131 @@ const bot = new AuthBot(conversationState, userState, dialog);
 const server = restify.createServer();
 server.use(restify.plugins.bodyParser());
 
-server.listen(process.env.port || process.env.PORT || 3978, function() {
-    console.log(`\n${ server.name } listening to ${ server.url }`);
-    console.log('\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator');
-    console.log('\nTo talk to your bot, open the emulator select "Open Bot"');
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+  //console.log(`\n${server.name} listening to ${server.url}`);
+  console.log(`\n${server.name} listening to http://localhost:3978`);
+  console.log('\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator');
+  console.log('\nTo talk to your bot, open the emulator select "Open Bot"');
 });
 
-// Listen for incoming requests.
+// HTTP / Pagina Principal
+server.get('/', (req, res, next) => {
+
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write(`
+      
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <script
+        crossorigin="anonymous"
+        src="https://cdn.botframework.com/botframework-webchat/latest/webchat.js"
+      ></script>
+      <style>
+        html,
+        body {
+            height: 100%;
+            background-image: linear-gradient( #343541,#525468);
+            color: antiquewhite;
+            font-family: 'Segoe UI', Calibri, sans-serif;
+        }
+  
+        body {
+          padding-left: 5px;
+        }
+  
+        #webchat {
+          height: 85%;
+          width: 100%;
+        }
+        .webchat__stacked-layout__main{
+          white-space: break-spaces;
+          
+        }
+        .webchat__stacked-layout--from-user{
+          background-color: rgba(32,33,35, .2);
+        }
+        
+      </style>
+    </head>
+    <body>
+      
+      <h1><img src='https://logos-world.net/wp-content/uploads/2021/02/Microsoft-Azure-Emblem.png' height="40">ChatGPT - REP</h1>
+      <!-- <pre>version 20231030 | model: ChatGPT (turbo) | API: Chat Completion API | max_tokens: 800 | temperature: 0.7 | Speech input enabled: false | Speech language: N/A</pre>  -->
+      <div style="" id="webchat" role="main"></div>
+      <script>
+        // Set  the CSS rules.
+        const styleSet = window.WebChat.createStyleSet({
+            bubbleBackground: 'transparent',
+            bubbleBorderColor: 'darkslategrey',
+            bubbleBorderRadius: 5,
+            bubbleBorderStyle: 'solid',
+            bubbleBorderWidth: 0,
+            bubbleTextColor: 'antiquewhite',
+  
+            userAvatarBackgroundColor: 'rgba(53, 55, 64, .3)',
+            bubbleFromUserBackground: 'transparent', 
+            bubbleFromUserBorderColor: '#E6E6E6',
+            bubbleFromUserBorderRadius: 5,
+            bubbleFromUserBorderStyle: 'solid',
+            bubbleFromUserBorderWidth: 0,
+            bubbleFromUserTextColor: 'antiquewhite',
+  
+            notificationText: 'white',
+  
+            bubbleMinWidth: 400,
+            bubbleMaxWidth: 720,
+  
+            botAvatarBackgroundColor: 'antiquewhite',
+            avatarBorderRadius: 2,
+            avatarSize: 40,
+  
+            rootHeight: '100%',
+            rootWidth: '100%',
+            backgroundColor: 'rgba(70, 130, 180, .2)',
+  
+            hideUploadButton: 'true'
+        });
+  
+        // After generated, you can modify the CSS rules.
+        // Change font family and weight. 
+        styleSet.textContent = {
+            ...styleSet.textContent,
+            fontWeight: 'regular'
+        };
+  
+      // Set the avatar options. 
+        const avatarOptions = {
+            botAvatarInitials: '.',
+            userAvatarInitials: 'Me',
+            botAvatarImage: 'https://dwglogo.com/wp-content/uploads/2019/03/1600px-OpenAI_logo-1024x705.png',            
+            };
+
+        window.WebChat.renderWebChat(
+          {
+            directLine: window.WebChat.createDirectLine({
+              token: '` + process.env.DIRECT_LINE_TOKEN + `'
+            }),
+            styleSet, styleOptions: avatarOptions
+          },
+          document.getElementById('webchat')
+        );
+      </script>
+        
+    </body>
+  </html>
+      `);
+  res.end();
+  return next();
+});
+
+// Enpoint Principal Azure Bot.
 server.post('/api/messages', async (req, res) => {
-    // Route received a request to adapter for processing
-    await adapter.process(req, res, (context) => bot.run(context));
+  await adapter.process(req, res, (context) => bot.run(context));
 });
 
+// Endpoint Test
 server.post('/api/test', (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
-    //this is a callback function
-    res.writeHead(200);
-    res.end('Hello Azure');
-  }));
+  res.writeHead(200);
+  res.end('Hello Azure');
+}));
